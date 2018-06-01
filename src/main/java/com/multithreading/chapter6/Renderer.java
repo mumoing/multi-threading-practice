799a92c0b2b89433c2ca763032e7c04d5d47cd55
wangbo <wangbo>
2018-06-01 15:02:30 +0800

    md test

--- src/main/java/com/multithreading/chapter6/Renderer.java
+++ src/main/java/com/multithreading/chapter6/Renderer.java
@@ -3,6 +3,7 @@ package com.multithreading.chapter6;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.*;
+import java.util.concurrent.locks.Lock;
 
 /**
  * CompletionService:Executor+BlockingQueue
